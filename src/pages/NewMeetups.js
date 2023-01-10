794693afc744e7065bbe9eb825ea3331bda0cdd5
Saheedatt <afolabisaheedat1@gmail.com>
2023-01-10 19:06:38 +0100

    Sixth commit-2

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,8 +1,24 @@
+import { useNavigate } from 'react-router-dom'
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage(){
+    const navigate = useNavigate();
+
+
     function addMeetupHandler(meetupData){
-        fetch('');
+        fetch(
+            'https://my-meetup-app-dd2e4-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST', 
+                body: JSON.stringify(meetupData),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(()=>{
+            navigate('/', {replace: true});
+        });
     }
     return(
     <section> <h1>Add New Meetup</h1>
